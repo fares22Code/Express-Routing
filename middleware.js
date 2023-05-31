30256dd0fe27827db4b31b9e1b819bcba00ce4d6
fares22Code <124197202+fares22Code@users.noreply.github.com>
2023-05-31 16:51:56 +0100

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -3,7 +3,7 @@ function checkTime(req,res,next) {
     const day = date.getDay()
     const hour = date.getHours()
 
-    if(day >= 1 && day <= 5  ) next()
+    if(day >= 1 && day <= 5  && hour >= 9 && hour <= 17) next()
     else res.send("hors time")
 
 }
